6a5cec70022d17da0f41883737ee17f4312548a6
lenglara <131978798+lenglara@users.noreply.github.com>
2025-02-02 14:26:05 +0100

    Enable animation control of right head via script

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,30 +7,56 @@ using UnityEngine.Serialization;
 
 public class AnimationController : MonoBehaviour
 {
-    [FormerlySerializedAs("animator")] public Animator rightHeadAnimator;
+    [SerializeField] public Animator rightHeadAnimator;
+    // [SerializeField] public Animator leftHeadAnimator;
 
+    //private bool _talkingLeft;
+    private bool _talkingRight;
+    
+    private int _currentState;
+    private static readonly int IdleR = Animator.StringToHash("IdleAnimation");
+    private static readonly int TalkingR = Animator.StringToHash("RightHeadAnimation");
+    // private static readonly int TalkingL = Animator.StringToHash("LeftHeadAnimation");
+    
     void Start()
     {
         // Stelle sicher, dass der Animator korrekt verknüpft ist
         if (rightHeadAnimator == null)
         {
-            rightHeadAnimator = GetComponent<Animator>();
+            Debug.Log("rightHeadAnimator is null");
         }
+
+        _talkingRight = false;
     }
 
     void Update()
     {
         // Beispiel: Animation starten, wenn die Leertaste gedrückt wird
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            rightHeadAnimator.SetBool("Talking", true);
+            //rightHeadAnimator.SetBool("Talking", true);
+
+            _talkingRight = false;
         }
 
         // Beispiel: Animation stoppen, wenn die Taste "S" gedrückt wird
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rightHeadAnimator.SetBool("Talking", false);
+            // rightHeadAnimator.SetBool("Talking", false);
+
+            _talkingRight = true;
         }
+        
+        var state = GetState();
+        
+        if (state == _currentState) return;
+        rightHeadAnimator.CrossFade(state, 0, 0);
+        _currentState = state;
+    }
+    
+    private int GetState() {
+        if (_talkingRight) return TalkingR;
+        return IdleR;
     }
 }
 
