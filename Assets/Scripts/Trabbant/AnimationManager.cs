acecff3c7855bedb24b87029c3e58c7b74a7219f
markaa99 <sinoislino@gmail.com>
2025-02-05 16:44:32 +0100

    bugfixes, final version

--- Assets/Scripts/Trabbant/AnimationManager.cs
+++ Assets/Scripts/Trabbant/AnimationManager.cs
@@ -9,49 +9,87 @@ public class AnimationManager : MonoBehaviour
     [SerializeField] WallSwitch wallSwitch;
     [SerializeField] GameObject car;
     [SerializeField] ParticleSystem[] particles;
+    public AudioSource audioSource;  
+    public AudioClip introClip;
+    public AudioSource wallRumble;
+    public AudioSource carAudioSource;
+    public float waitTimeReduce;
     
     void Start()
     {
         car.SetActive(false);
     }
     
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PlayFirstAnimation();
-        }
-    }
-
     public void PlayFirstAnimation()
     {
-        wallAnimator.SetTrigger("Rise Walls");
+        
+        StartCoroutine(PlayFirstAnimationCoroutine());
+    }
+    
+    private IEnumerator PlayIntroTextCoroutine()
+    {
+        audioSource.clip = introClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(2f);
+        StopParticles();
+        yield return new WaitForSeconds(introClip.length - 3);
+
+        // Dann startet die erste Animation mit einer Verzögerung von 2 Sekunden
+        StartCoroutine(StopParticlesCoroutine());
+    }
+
+    private IEnumerator PlayFirstAnimationCoroutine()
+    {
+        wallRumble.Play();
+        // Partikel abspielen
         foreach (ParticleSystem particle in particles)
         {
             particle.Play();
         }
-        
+
+        wallAnimator.SetTrigger("Rise Walls");
+        yield return new WaitForSeconds(wallRumble.clip.length);
+
+        StartCoroutine(PlayIntroTextCoroutine());
     }
-    
+
     public void StopParticles()
     {
         foreach (ParticleSystem particle in particles)
         {
             particle.Stop();
         }
+    }
+
+    private IEnumerator StopParticlesCoroutine()
+    {
+        // 2 Sekunden warten
+        yield return new WaitForSeconds(1f);
+        // Zur nächsten Animation übergehen
         TransitionToSecondAnimation();
     }
 
     public void TransitionToSecondAnimation()
     {
+        StartCoroutine(TransitionToSecondAnimationCoroutine());
+    }
+
+    private IEnumerator TransitionToSecondAnimationCoroutine()
+    {
+        // Wand wechseln und Auto aktivieren
         wallSwitch.SwitchWall();
         car.SetActive(true);
-        WaitForSeconds wait = new WaitForSeconds(2f);
+
+        // 2 Sekunden warten
+        yield return new WaitForSeconds(1f);
+
+        // Nächste Animation starten
         PlaySecondAnimation();
     }
 
     public void PlaySecondAnimation()
     {
+        carAudioSource.Play();
         carAnimator.SetTrigger("StartAnimation");
     }
 }
